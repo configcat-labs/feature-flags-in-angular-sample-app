207bd884be7a80321a17d1faa701324b959571ec
Alexandru Mircean <alexandru@logmycare.co.uk>
2024-08-07 19:00:44 +0300

    try app

--- votingPage/src/app/app.component.ts
+++ votingPage/src/app/app.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
-import * as configcat from "configcat-js";
+import * as configcat from 'configcat-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'votingPage';
-  votingFeatureEnabled:boolean = false;
+  votingFeatureEnabled: boolean = false;
 
   constructor() {
-    let configCatClient = configcat.createClient("YOUR_SDK_KEY");
-    configCatClient.getValueAsync("votingOpen",  false)
-    .then( value => {
+    let configCatClient = configcat.createClient(
+      'configcat-sdk-1/cIXcCIFM50eL1D7-bo-KNw/AoEy1jmHOUCAxu7cBz0ypA'
+    );
+    console.log(configCatClient.getValueAsync('grandFeature', false));
+    configCatClient.getValueAsync('grandFeature', false).then((value) => {
       this.votingFeatureEnabled = value;
     });
   }
